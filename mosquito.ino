70d6679f537ea5a92246ea210cac9f5c9b1b7e56
Big Mike <michael.larsen.jorgensen@gmail.com>
2025-11-09 03:02:42 -0500

    Create mosquito.ino

---
+++ mosquito.ino
@@ -0,0 +1,130 @@
+//task manager-> services-> mosquito-> start
+//windows defender advanced->MQTT test with port->1883
+//cmd window as admin
+//netsh interface portproxy add v4tov4 listenaddress=10.4.16.195 listenport=1883 connectaddress=127.0.0.1 connectport=1883
+//netsh interface portproxy show all
+//run serial monitor at 115200 baud rate
+
+#ifdef ESP8266
+  #include <ESP8266WiFi.h>     /* WiFi library for ESP8266 */
+#else
+  #include <WiFi.h>            /* WiFi library for ESP32 */
+#endif
+#include <Wire.h>
+#include <PubSubClient.h>
+#include "DHT.h"             /* DHT sensor library */
+
+#define DHTPIN 4
+#define DHTTYPE DHT22 // DHT 22
+DHT dht(DHTPIN, DHTTYPE);
+
+#define wifi_ssid "CyberSec" //router specific
+#define wifi_password "cis401303"
+#define mqtt_server "mqtt.eclipseprojects.io" //connecting to the public eclipse server
+
+#define humidity_topic "sensor/DHT22/humidity" //topic name for humidity
+#define temperature_celsius_topic "sensor/DHT22/temperature_celsius" //topic name for celsius
+#define temperature_fahrenheit_topic "sensor/DHT22/temperature_fahrenheit" //topic name for fahrenheit
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void setup() {
+  Serial.begin(115200);
+  // dht.begin();
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+}
+
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(wifi_ssid);
+
+  WiFi.begin(wifi_ssid, wifi_password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+        Serial.print("Attempting MQTT connection...");
+
+  if (client.connect("ESP8266Client")) {
+      Serial.println("connected");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
+    }
+  }
+}
+
+
+void loop() {
+
+      if (!client.connected()) {
+        reconnect();
+      }
+      client.loop();
+
+      // Wait a few seconds between measurements.
+      delay(2000);
+
+      // Reading temperature or humidity takes about 250 milliseconds!
+      // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+      float h = dht.readHumidity();
+      // Read temperature as Celsius (the default)
+      float t = dht.readTemperature();
+      // Read temperature as Fahrenheit (isFahrenheit = true)
+      float f = dht.readTemperature(true);
+
+      // Check if any reads failed and exit early (to try again).
+     // if (isnan(h) || isnan(t) || isnan(f)) {
+     // Serial.println("Failed to read from DHT sensor!");
+      //return;
+      //}
+
+      // Compute heat index in Fahrenheit (the default)
+      float hif = dht.computeHeatIndex(f, h);
+      // Compute heat index in Celsius (isFahreheit = false)
+      float hic = dht.computeHeatIndex(t, h, false);
+      Serial.print("Humidity: ");
+      Serial.print(h);
+      Serial.print(" %\t");
+      Serial.print("Temperature: ");
+      Serial.print(t);
+      Serial.print(" *C ");
+      Serial.print(f);
+      Serial.print(" *F\t");
+      Serial.print("Heat index: ");
+      Serial.print(hic);
+      Serial.print(" *C ");
+      Serial.print(hif);
+      Serial.println(" *F");
+
+      Serial.print("Temperature in Celsius:");
+      Serial.println(String(t).c_str());
+      client.publish(temperature_celsius_topic, String(t).c_str(), true);
+
+      Serial.print("Temperature in Fahrenheit:");
+      Serial.println(String(f).c_str());
+      client.publish(temperature_fahrenheit_topic, String(f).c_str(), true);
+
+      Serial.print("Humidity:");
+      Serial.println(String(h).c_str());
+      client.publish(humidity_topic, String(h).c_str(), true);
+
+}
